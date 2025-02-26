7d5e27c2a579ffa8df53b569fce5050c6949a9b4
HIENLTH @ HCMUE <hyhien@gmail.com>
2025-02-26 08:31:49 +0700

    Demo sync vs async

---
+++ FirstProject/FirstProject/Controllers/DemoController.cs
@@ -0,0 +1,33 @@
+﻿using FirstProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FirstProject.Controllers
+{
+    public class DemoController : Controller
+    {
+        public async Task<IActionResult> Test2()
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            var demo = new Demo();
+            var a = demo.AsyncA();
+            var b = demo.AsyncB();
+            var c = demo.AsyncC();
+            await a; await b; await c;            
+            sw.Stop();
+            return Content($"Chạy hết {sw.ElapsedMilliseconds} ms");
+        }
+        public IActionResult Test1()
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            var demo = new Demo();
+            demo.A();
+            demo.B();
+            demo.C();
+            sw.Stop();
+            return Content($"Chạy hết {sw.ElapsedMilliseconds} ms");
+        }
+    }
+}
---
+++ FirstProject/FirstProject/Models/Demo.cs
@@ -0,0 +1,32 @@
+﻿namespace FirstProject.Models
+{
+    public class Demo
+    {
+        public int A()
+        {
+            Thread.Sleep(2000);
+            return new Random().Next();
+        }
+        public string B()
+        {
+            Thread.Sleep(5000);
+            return "HELLO";
+        }
+        public void C() { Thread.Sleep(3000); }
+
+        public async Task<int> AsyncA()
+        {
+            await Task.Delay(2000);
+            return new Random().Next();
+        }
+        public async Task<string> AsyncB()
+        {
+            await Task.Delay(5000);
+            return "AAA";
+        }
+        public async Task AsyncC()
+        {
+            await Task.Delay(3000);
+        }
+    }
+}

d3a01bb47ac1dec754d4ec6aa9b70c9d2f0f8622
HIENLTH @ HCMUE <hyhien@gmail.com>
2025-04-02 09:02:05 +0700

    Lab07

---
+++ MyEstoreProject/MyEstoreProject/Controllers/HangHoaController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyEstoreProject.Entities;
+using MyEstoreProject.Models;
+
+namespace MyEstoreProject.Controllers
+{
+    public class HangHoaController : Controller
+    {
+        private readonly MyeStoreContext _ctx;
+        public HangHoaController(MyeStoreContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string? Keyword, double? FromPrice, double? ToPrice)
+        {
+            var result = _ctx.HangHoas.AsQueryable();
+            if (!string.IsNullOrEmpty(Keyword))
+            {
+                result = result.Where(hh => hh.TenHh.Contains(Keyword));
+            }
+            if (FromPrice.HasValue)
+            {
+                result = result.Where(hh => hh.DonGia >= FromPrice);
+            }
+            if (ToPrice.HasValue)
+            {
+                result = result.Where(hh => hh.DonGia <= ToPrice);
+            }
+            var data = result.Select(hh => new HangHoaVM
+            {
+                MaHh = hh.MaHh,
+                TenHh = hh.TenHh,
+                DonGia = hh.DonGia ?? 0,
+                Hinh = hh.Hinh,
+                TenLoai = hh.MaLoaiNavigation.TenLoai
+            }).ToList();
+            return View(data);
+        }
+    }
+}
---
+++ MyEstoreProject/MyEstoreProject/Models/HangHoaVM.cs
@@ -0,0 +1,11 @@
+﻿namespace MyEstoreProject.Models
+{
+    public class HangHoaVM
+    {
+        public int MaHh { get; set; }
+        public string TenHh { get; set; } = null!;
+        public string Hinh { get; set; } = null!;
+        public double DonGia { get; set; }
+        public string TenLoai { get; set; }
+    }
+}
